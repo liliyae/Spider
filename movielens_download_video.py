14f6bbf60a69f1cd7c99981f6015dec283373f95
liliyae <58354216+liliyae@users.noreply.github.com>
2021-08-20 09:46:07 +0800

    Update movielens_download_video.py

--- movielens_download_video.py
+++ movielens_download_video.py
@@ -11,8 +11,33 @@ import random
 import json
 import urllib.request
 
+options = webdriver.ChromeOptions()
+# 添加UA
+options.add_argument(
+    'user-agent="MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"')
+# 指定浏览器分辨率
+options.add_argument('window-size=1920x3000')
+# 谷歌文档提到需要加上这个属性来规避bug
+options.add_argument('--disable-gpu')
+# 隐藏滚动条, 应对一些特殊页面
+options.add_argument('--hide-scrollbars')
+# 不加载图片, 提升速度
+options.add_argument('blink-settings=imagesEnabled=false')
+# 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+options.add_argument('--headless')
+# 以最高权限运行
+options.add_argument('--no-sandbox')
+# 设置开发者模式启动，该模式下webdriver属性为正常值
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+# 禁用浏览器弹窗
+prefs = {
+    'profile.default_content_setting_values': {
+        'notifications': 2
+    }
+}
+options.add_experimental_option('prefs', prefs)
 
-browser = webdriver.Chrome(executable_path="D:\\tools\\chromedriver.exe")
+browser = webdriver.Chrome(executable_path="D:\\tools\\chromedriver.exe",chrome_options=options)
 browser.maximize_window()  # 最大化窗口
 wait = WebDriverWait(browser, 5) # 等待加载10s
 
@@ -49,3 +74,4 @@ def get_page_index(url,index):
 
 
 login()
+
